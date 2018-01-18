f2efd8013ea0fea3cd57fc3ea1ca90aa9aeeb7a1
Michael Reichert <michael.reichert@geofabrik.de>
2018-01-18 16:06:06 +0100

    bugfixing turn angle treatment: consider all edge pairs at a node

--- src/main/java/de/geofabrik/sncf_railway_routing/RailwayHopper.java
+++ src/main/java/de/geofabrik/sncf_railway_routing/RailwayHopper.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import com.graphhopper.reader.osm.GraphHopperOSM;
 import com.graphhopper.routing.util.EncodingManager;
 import com.graphhopper.routing.util.FlagEncoder;
+import com.graphhopper.routing.weighting.TurnWeighting;
 import com.graphhopper.storage.GraphHopperStorage;
 import com.graphhopper.storage.TurnCostExtension;
 import com.graphhopper.util.AngleCalc;
@@ -20,6 +21,7 @@ import com.graphhopper.util.PointList;
 public class RailwayHopper extends GraphHopperOSM {
     public RailwayHopper(final CmdArgs args) {
         setEncodingManager(new EncodingManager(new RailFlagEncoder(5, 5, 0), new RailFlagEncoder(5, 5, 1)));
+        super.init(args);
     }
 
     private double getAngle(double or1, double or2) {
@@ -35,6 +37,9 @@ public class RailwayHopper extends GraphHopperOSM {
         GraphHopperStorage ghs = getGraphHopperStorage();
         TurnCostExtension tcs = (TurnCostExtension) ghs.getExtension();
         for (FlagEncoder encoder : getEncodingManager().fetchEdgeEncoders()) {
+            if (!encoder.supports(TurnWeighting.class)) {
+                continue;
+            }
             long tflags = encoder.getTurnFlags(true, 0);
             EdgeExplorer explorer = ghs.createEdgeExplorer();
             int nodes = ghs.getNodes();
@@ -44,8 +49,10 @@ public class RailwayHopper extends GraphHopperOSM {
                 }
                 EdgeIterator iter = explorer.setBaseNode(start);
                 double prevOrientation = Double.MAX_VALUE;
-                int prevBaseNode = Integer.MAX_VALUE;
-                int prevAdjNode = Integer.MAX_VALUE;
+                int prevEdgeId = Integer.MAX_VALUE;
+                // We have to cache the first edge to have it available when we get the last edge.
+                double firstOrientation = Double.MAX_VALUE;
+                int firstEdgeId = Integer.MAX_VALUE;
                 while (iter.next()) {
                     PointList points = null;
                     // get geometry
@@ -53,26 +60,32 @@ public class RailwayHopper extends GraphHopperOSM {
                     // get orientation
                     double lon1 = points.getLon(0);
                     double lat1 = points.getLat(0);
-                    double lon2 = points.getLon(points.size() - 1);
-                    double lat2 = points.getLat(points.size() - 1);
+                    double lon2 = points.getLon(1);
+                    double lat2 = points.getLat(1);
                     double orientation = angleCalc.calcOrientation(lat1, lon1, lat2, lon2);
                     if (prevOrientation == Double.MAX_VALUE
-                            || prevBaseNode == Integer.MAX_VALUE
-                            || prevAdjNode == Integer.MAX_VALUE) {
+                            || prevEdgeId == Integer.MAX_VALUE) {
                         prevOrientation = orientation;
-                        prevBaseNode = iter.getBaseNode();
-                        prevAdjNode = iter.getAdjNode();
+                        firstOrientation = orientation;
+                        prevEdgeId = iter.getEdge();
+                        firstEdgeId = iter.getEdge();
                         continue;
                     }
                     // get difference
                     double diff = getAngle(orientation, prevOrientation);
-                    if (diff < 135 || diff > 225) {
-                        System.err.println("remove");
-                        tcs.addTurnInfo(getEdge(ghs, prevBaseNode, prevAdjNode).getEdge(),
-                                iter.getBaseNode(), iter.getEdge(), tflags);
+                    if (diff < 0.75 * Math.PI || diff > 1.25 * Math.PI) {
+                        // We have to forbid this turn in both directions.
+                        tcs.addTurnInfo(prevEdgeId, iter.getBaseNode(), iter.getEdge(), tflags);
+                        tcs.addTurnInfo(iter.getEdge(), iter.getBaseNode(), prevEdgeId, tflags);
                     }
-                    prevBaseNode = iter.getBaseNode();
-                    prevAdjNode = iter.getAdjNode();
+                    prevEdgeId = iter.getEdge();
+                    prevOrientation = orientation;
+                }
+                // compare last and first edge
+                double diff = getAngle(prevOrientation, firstOrientation);
+                if (diff < 0.75 * Math.PI || diff > 1.25 * Math.PI) {
+                    tcs.addTurnInfo(prevEdgeId, iter.getBaseNode(), firstEdgeId, tflags);
+                    tcs.addTurnInfo(firstEdgeId, iter.getBaseNode(), prevEdgeId, tflags);
                 }
             }
         }
